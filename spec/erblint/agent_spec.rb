87af0c3fdca33430ee531ee4d122bca54ce82c18
aki <lala.akira@gmail.com>
2025-08-28 19:50:47 +0900

    first commit

---
+++ spec/erblint/agent_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Erblint::Agent do
+  it "has a version number" do
+    expect(Erblint::Agent::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
